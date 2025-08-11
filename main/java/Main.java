7c1497cebac4d2470deeee6cc26ade0f92982d2e
junseokPP <pjs981130@gmail.com>
2025-08-11 20:01:56 +0900

    2step

--- main/java/Main.java
+++ main/java/Main.java
@@ -4,7 +4,23 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("== 명언 앱 ==");
-        System.out.print("명령) ");
-        sc.nextLine();
+
+
+       while(true){
+           System.out.print("명령) ");
+           String cmd = sc.nextLine();
+
+           switch (cmd) {
+               case "등록" ->{
+                   System.out.print("명언 : ");
+                   sc.nextLine();
+                   System.out.print("작가 : ");
+                   sc.nextLine();
+               }
+               case "종료" ->{
+                   return;
+               }
+           }
+       }
     }
 }
