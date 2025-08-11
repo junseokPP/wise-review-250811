fef90d5bb035fc6013b695d0afa5ea07e03e9260
junseokPP <pjs981130@gmail.com>
2025-08-11 20:03:28 +0900

    3step

--- main/java/Main.java
+++ main/java/Main.java
@@ -16,6 +16,8 @@ public class Main {
                    sc.nextLine();
                    System.out.print("작가 : ");
                    sc.nextLine();
+
+                   System.out.println("1번 명언이 등록되었습니다.");
                }
                case "종료" ->{
                    return;
