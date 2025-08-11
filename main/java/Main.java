3cf5be90b20b93f9c872b8fa7feeefd31a318c18
junseokPP <pjs981130@gmail.com>
2025-08-11 20:19:32 +0900

    5step

--- main/java/Main.java
+++ main/java/Main.java
@@ -3,22 +3,37 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int lastId = 0;
-        System.out.println("== 명언 앱 ==");
 
+        int lastId = 0,lastIndex=0;
+        String saying;
+        String author;
+        WiseSaying[] wiseSaying = new WiseSaying[100];
+
+        System.out.println("== 명언 앱 ==");
 
        while(true){
            System.out.print("명령) ");
            String cmd = sc.nextLine();
 
+
            switch (cmd) {
                case "등록" ->{
                    System.out.print("명언 : ");
-                   sc.nextLine();
+                   saying = sc.nextLine();
                    System.out.print("작가 : ");
-                   sc.nextLine();
+                   author = sc.nextLine();
                    lastId++;
+
+                   wiseSaying[lastIndex++] = new WiseSaying(lastId,saying,author);
                    System.out.println("%d번 명언이 등록되었습니다.".formatted(lastId));
+               }
+               case "목록" ->{
+                   System.out.println("번호 / 작가 / 명언");
+                   System.out.println("----------------------");
+                   for(int i=0;i<lastIndex;i++){
+                       WiseSaying ws = wiseSaying[i];
+                       System.out.println("%d / %s / %s".formatted(ws.id,ws.saying,ws.author));
+                   }
                }
                case "종료" ->{
                    return;
---
+++ main/java/WiseSaying.java
@@ -0,0 +1,13 @@
+public class WiseSaying {
+    int id;
+    String saying;
+    String author;
+
+    public WiseSaying(){}
+
+    public WiseSaying(int id, String saying, String author) {
+        this.id = id;
+        this.saying = saying;
+        this.author = author;
+    }
+}
