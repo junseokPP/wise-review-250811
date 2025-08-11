54d7c9a62a9cf16dd92b0bb6dfbc57687c6c9400
junseokPP <pjs981130@gmail.com>
2025-08-11 20:05:00 +0900

    4step

--- main/java/Main.java
+++ main/java/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int lastId = 0;
         System.out.println("== 명언 앱 ==");
 
 
@@ -16,8 +17,8 @@ public class Main {
                    sc.nextLine();
                    System.out.print("작가 : ");
                    sc.nextLine();
-
-                   System.out.println("1번 명언이 등록되었습니다.");
+                   lastId++;
+                   System.out.println("%d번 명언이 등록되었습니다.".formatted(lastId));
                }
                case "종료" ->{
                    return;
