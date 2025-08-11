63c82edba8907b2bebe2990bd1ac462ef175569e
junseokPP <pjs981130@gmail.com>
2025-08-11 19:58:23 +0900

    1step

--- main/java/Main.java
+++ main/java/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("== 명언 앱 ==");
+        System.out.print("명령) ");
+        sc.nextLine();
     }
 }
