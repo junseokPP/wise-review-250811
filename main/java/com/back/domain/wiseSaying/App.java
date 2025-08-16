894bb1636f5e42d225692ca3c71c43e2bb15b76a
junseokPP <pjs981130@gmail.com>
2025-08-16 19:55:42 +0900

    step3 - 11단계 - WiseSayingController

--- main/java/com/back/domain/wiseSaying/App.java
+++ main/java/com/back/domain/wiseSaying/App.java
@@ -1,15 +1,16 @@
 package com.back.domain.wiseSaying;
 
 import com.back.domain.wiseSaying.controller.SystemController;
+import com.back.domain.wiseSaying.controller.WiseSayingController;
 
 import java.util.*;
 
 public class App {
         private Scanner sc = new Scanner(System.in);
-        private int lastId = 0;
-        private List<WiseSaying> wiseSayings = new ArrayList<>();
+
         private Map<String , String> paramMap = new HashMap<>();
         private SystemController systemController = new SystemController();
+        private WiseSayingController wiseSayingController = new WiseSayingController(sc);
 
     public void run(){
 
@@ -22,13 +23,13 @@ public class App {
             Rq rq = new Rq(cmd);
             String actionName = rq.getActionName();
             if (actionName.equals("등록")) {
-                actionWrite();
+                wiseSayingController.actionWrite();
             } else if (actionName.equals("목록")) {
-                actionList();
+                wiseSayingController.actionList();
             } else if (actionName.startsWith("삭제")) {
-                actionDelete(rq);
+                wiseSayingController.actionDelete(rq);
             } else if (actionName.startsWith("수정")) {
-                actionModify(rq);
+                wiseSayingController.actionModify(rq);
             } else if (actionName.equals("종료")) {
                 systemController.exit();
                 return;
@@ -36,99 +37,5 @@ public class App {
         }
     }
 
-    private void actionModify(Rq rq) {
 
-
-        int id = rq.getParamAsInt("id",-1);
-        WiseSaying wiseSaying  = findByIdOrNull(id);
-
-        if(wiseSaying == null){
-            System.out.println("%d 명언은 존재하지 않습니다.".formatted(id));
-            return;
-        }
-
-
-        System.out.println("명언(기존) : %s".formatted(wiseSaying.getSaying()));
-        System.out.print("명언 :");
-        String newSaying = sc.nextLine();
-        System.out.println("작가(기존) : %s".formatted(wiseSaying.getAuthor()));
-        System.out.print("작가 :");
-        String newAuthor = sc.nextLine();
-
-        modify(wiseSaying,newSaying,newAuthor);
-    }
-
-    private void modify(WiseSaying wiseSaying, String newSaying, String newAuthor) {
-        wiseSaying.setSaying(newSaying);
-        wiseSaying.setAuthor(newAuthor);
-    }
-
-    private WiseSaying findByIdOrNull(int id){
-        return wiseSayings.stream()
-                .filter(wiseSaying -> wiseSaying.getId() == id)
-                .findFirst()
-                .orElse(null);
-    }
-
-//    private int findIndexById(int id){
-//        return IntStream.range(0, wiseSayings.size())
-//                .filter(i -> wiseSayings.get(i).getId() == id)
-//                .findFirst()
-//                .orElse(-1);
-//
-//    }
-
-    private void actionDelete(Rq rq) {
-
-        int id =   rq.getParamAsInt("id",-1);
-        boolean result = delete(id);
-
-        if(result){
-            System.out.println("1번 명언이 삭제되었습니다.");
-        }
-        else{
-            System.out.println("%d번 명언은 존재하지 않습니다.".formatted(id));
-        }
-    }
-
-    private boolean delete(int id){
-        //for문으로 break를 거는게 성능은 더좋다
-        //removeIf를 사용하는 이유는 가독성 때문
-        return wiseSayings.removeIf(w -> w.getId() == id);
-    }
-
-    private void actionWrite(){
-
-        System.out.print("명언 : ");
-        String saying = sc.nextLine();
-        System.out.print("작가 : ");
-        String author = sc.nextLine();
-
-        WiseSaying wiseSaying = write(saying,author);
-
-        System.out.println("%d번 명언이 등록되었습니다.".formatted(wiseSaying.getId()));
-    }
-
-    private WiseSaying write(String saying, String author){
-        lastId++;
-        WiseSaying wiseSaying = new WiseSaying(lastId,saying,author);
-        wiseSayings.add(wiseSaying);
-        return wiseSaying;
-    }
-
-    private void actionList(){
-        System.out.println("번호 / 작가 / 명언");
-        System.out.println("----------------------");
-
-        List<WiseSaying> wiseSayings = findListDesc();
-
-        for(WiseSaying wiseSaying : wiseSayings){
-            System.out.println("%d / %s / %s".formatted(wiseSaying.getId(),wiseSaying.getAuthor(),wiseSaying.getSaying()));
-        }
-    }
-
-    private List<WiseSaying> findListDesc(){
-
-        return wiseSayings.reversed();
-    }
 }
