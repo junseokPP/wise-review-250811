3d881a18593a88175853671a67e71da3144eb0e3
junseokPP <pjs981130@gmail.com>
2025-08-16 20:12:50 +0900

    step3 - 11단계 - WiseSayingRepository

---
+++ main/java/com/back/domain/wiseSaying/repository/WiseSayingRepository.java
@@ -0,0 +1,33 @@
+package com.back.domain.wiseSaying.repository;
+
+import com.back.domain.wiseSaying.WiseSaying;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WiseSayingRepository {
+
+
+    private List<WiseSaying> wiseSayings = new ArrayList<>();
+
+    public WiseSaying findByIdOrNull(int id){
+        return wiseSayings.stream()
+                .filter(wiseSaying -> wiseSaying.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
+
+    public boolean delete(int id){
+        //for문으로 break를 거는게 성능은 더좋다
+        //removeIf를 사용하는 이유는 가독성 때문
+        return wiseSayings.removeIf(w -> w.getId() == id);
+    }
+
+    public void save(WiseSaying wiseSaying){
+        wiseSayings.add(wiseSaying);
+    }
+
+    public List<WiseSaying> findListDesc(){
+        return wiseSayings.reversed();
+    }
+}
--- main/java/com/back/domain/wiseSaying/service/WiseSayingService.java
+++ main/java/com/back/domain/wiseSaying/service/WiseSayingService.java
@@ -1,6 +1,7 @@
 package com.back.domain.wiseSaying.service;
 
 import com.back.domain.wiseSaying.WiseSaying;
+import com.back.domain.wiseSaying.repository.WiseSayingRepository;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,35 +9,31 @@ import java.util.List;
 public class WiseSayingService {
 
     private int lastId = 0;
-    private List<WiseSaying> wiseSayings = new ArrayList<>();
+    WiseSayingRepository wiseSayingRepository = new WiseSayingRepository();
 
     public void modify(WiseSaying wiseSaying, String newSaying, String newAuthor) {
         wiseSaying.setSaying(newSaying);
         wiseSaying.setAuthor(newAuthor);
     }
 
-    public boolean delete(int id){
-        //for문으로 break를 거는게 성능은 더좋다
-        //removeIf를 사용하는 이유는 가독성 때문
-        return wiseSayings.removeIf(w -> w.getId() == id);
-    }
-
     public WiseSaying write(String saying, String author){
+
         lastId++;
         WiseSaying wiseSaying = new WiseSaying(lastId,saying,author);
-        wiseSayings.add(wiseSaying);
+        wiseSayingRepository.save(wiseSaying);
+
         return wiseSaying;
     }
 
-    public WiseSaying findByIdOrNull(int id){
-        return wiseSayings.stream()
-                .filter(wiseSaying -> wiseSaying.getId() == id)
-                .findFirst()
-                .orElse(null);
+    public List<WiseSaying> findListDesc(){
+        return wiseSayingRepository.findListDesc();
     }
 
-    public List<WiseSaying> findListDesc(){
+    public WiseSaying getByIdOrNull(int id){
+        return wiseSayingRepository.findByIdOrNull(id);
+    }
 
-        return wiseSayings.reversed();
+    public boolean delete(int id){
+        return wiseSayingRepository.delete(id);
     }
 }
