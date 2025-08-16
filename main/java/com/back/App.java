ecc2d062d86c5311e32232437ffcc807a926b171
junseokPP <pjs981130@gmail.com>
2025-08-16 14:15:10 +0900

    6step - run()함수에 많은 코드들을 분리함

--- main/java/com/back/App.java
+++ main/java/com/back/App.java
@@ -3,14 +3,12 @@ package com.back;
 import java.util.Scanner;
 
 public class App {
+        Scanner sc = new Scanner(System.in);
+        int lastId = 0,lastIndex=0;
+        WiseSaying[] wiseSaying = new WiseSaying[100];
 
     public void run(){
-        Scanner sc = new Scanner(System.in);
 
-        int lastId = 0,lastIndex=0;
-        String saying;
-        String author;
-        WiseSaying[] wiseSaying = new WiseSaying[100];
 
         System.out.println("== 명언 앱 ==");
 
@@ -21,22 +19,10 @@ public class App {
 
             switch (cmd) {
                 case "등록" ->{
-                    System.out.print("명언 : ");
-                    saying = sc.nextLine();
-                    System.out.print("작가 : ");
-                    author = sc.nextLine();
-                    lastId++;
-
-                    wiseSaying[lastIndex++] = new WiseSaying(lastId,saying,author);
-                    System.out.println("%d번 명언이 등록되었습니다.".formatted(lastId));
+                    actionWrite();
                 }
                 case "목록" ->{
-                    System.out.println("번호 / 작가 / 명언");
-                    System.out.println("----------------------");
-                    for(int i=0;i<lastIndex;i++){
-                        WiseSaying ws = wiseSaying[i];
-                        System.out.println("%d / %s / %s".formatted(ws.id,ws.author,ws.saying));
-                    }
+                    actionList();
                 }
                 case "삭제?id=1" ->{
                     System.out.println("1번 명언이 삭제되었습니다.");
@@ -44,6 +30,34 @@ public class App {
                     return;
                 }
             }
+        }
+    }
+
+    public void actionWrite(){
+        String saying;
+        String author;
+
+        System.out.print("명언 : ");
+        saying = sc.nextLine();
+        System.out.print("작가 : ");
+        author = sc.nextLine();
+        lastId++;
+
+        wiseSaying[lastIndex++] = new WiseSaying(lastId,saying,author);
+        System.out.println("%d번 명언이 등록되었습니다.".formatted(lastId));
+    }
+
+    public void actionList(){
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("----------------------");
+        for(int i=0;i<lastIndex;i++){
+            WiseSaying ws = wiseSaying[i];
+
+            if(ws == null){
+                break;
+            }
+
+            System.out.println("%d / %s / %s".formatted(ws.id,ws.author,ws.saying));
         }
     }
 }
